a2d347763956853a62f581227bb7bed85f3cd9cc
LucasPerezz <perezlucas2609@gmail.com>
2022-12-11 15:31:47 -0300

    desafio 1

--- Desafio-1/index.js
+++ Desafio-1/index.js
@@ -8,12 +8,12 @@ class ProductManager {
 
 
     addProduct(product) {
-        if(this.products.findIndex(prod => prod.code === product.code) === -1) {
+        if(this.products.findIndex(prod => prod.code === product.code) === -1 && validate(product) === 1) {
             product = {...product, id: ProductManager.productId};
             this.products.push(product);
             ProductManager.productId++;
         } else {
-            console.log("Error, codigo repetido");
+            throw new Error("Error, codigo repetido");
         }
 
     }
@@ -25,10 +25,18 @@ class ProductManager {
     getProductById(id) {
         let productFound = this.products.find(prod => prod.id === id);
         if(productFound === undefined) {
-            console.log("Not found");
+            throw new Error("Not found");
         } else {
             return productFound;
         }
+    }
+}
+
+const validate = (product) => {
+    if(!product.title || !product.description || !product.price || !product.code || !product.thumbnail || !product.stock) {
+        throw new Error("TODOS los campos son obligatorios!");
+    } else {
+        return 1;
     }
 }
 
@@ -37,20 +45,32 @@ const classControl = new ProductManager();
 const product = {
     title: "silla",
     description: "Madera",
+    thumbnail: "https:www.awdawdadwads.com",
     price: 1300,
     code: 132,
     stock: 10
-}
+};
 
 const product2 = {
     title: "mesa",
     description: "Madera",
+    thumbnail: "https:www.awdawdadwads.com",
     price: 2000,
     code: 161,
     stock: 10
-}
+};
+
+const product3 = {
+    title: "Platos",
+    description: "Porcelana",
+    thumbnail: "https:www.awdawdadwads.com",
+    price: 2000,
+    code: 241,
+    stock: 13
+};
 
 classControl.addProduct(product);
 classControl.addProduct(product2);
+classControl.addProduct(product3);
 console.log(classControl.getProducts());
 console.log(classControl.getProductById(1));
